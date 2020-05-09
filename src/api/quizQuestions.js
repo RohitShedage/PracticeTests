9346bf8cca8af221e1ec18068137d0712d86901e
RohitShedage <rohitshedage@gmail.com>
2020-05-09 12:11:56 -0500

    new quiz

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,51 +1,160 @@
 const quizQuestions = [
   {
-    question: "18 X 19 = ",
+    question: "Select the correct option?",
     answers: [
       {
         type: "correct",
-        content: "342"
+        content: "4/7 = 12/21"
       },
       {
-        content: "432"
+        content: "4/9 = 9/14"
       },
       {
-        content: "162"
+        content: "4/7 = 12/14"
       }
     ]
   },
   {
-    question: "456 + 767",
+    question:
+      "The product of two numbers is 2880.One of the numbers is 30. Find the other.",
     answers: [
       {
         type: "correct",
-        content: "1223"
+        content: "96"
       },
       {
-        content: "1213"
+        content: "86400"
       },
       {
-        content: "1123"
+        content: "12200"
       }
     ]
   },
   {
-    question: "46 + 77",
+    question: "Convert 817 cm to m and cm?",
     answers: [
       {
         type: "correct",
-        content: "123"
+        content: "8m 17cm"
       },
       {
-        content: "113"
+        content: "81m 7cm"
       },
       {
-        content: "13"
+        content: "817m 0cm"
+      }
+    ]
+  },
+  {
+    question:
+      "Mr. Rao reaches his office at 9:30 am and leaves at 5:45 pm. How long does he stay in the office?",
+    answers: [
+      {
+        type: "correct",
+        content: "8hour 15minutes"
+      },
+      {
+        content: "3hour 45minutes"
+      },
+      {
+        content: "7hour 15minutes"
+      }
+    ]
+  },
+  {
+    question: "Which of the following statements is correct?",
+    answers: [
+      {
+        type: "correct",
+        content: "3/4 > 3/5"
+      },
+      {
+        content: "3/4 < 3/5"
+      },
+      {
+        content: "3/4 = 3/5"
+      }
+    ]
+  },
+  {
+    question: "3 weeks + October + 15 days?",
+    answers: [
+      {
+        type: "correct",
+        content: "9week 4days"
+      },
+      {
+        content: "66 days"
+      },
+      {
+        content: "April + June + 3 days"
+      }
+    ]
+  },
+  {
+    question: "Which of the following fractions are in ascending order?",
+    answers: [
+      {
+        type: "correct",
+        content: "2/3 , 3/4, 4/5"
+      },
+      {
+        content: "3/4 , 3/5, 3/7"
+      },
+      {
+        content: "5/7, 3/5, 7/9"
+      }
+    ]
+  },
+  {
+    question:
+      "How many 4-digits numbers are possible using 1, 0, 9 and 2 writing each digit only.?",
+    answers: [
+      {
+        type: "correct",
+        content: "18"
+      },
+      {
+        content: "24"
+      },
+      {
+        content: "12"
+      }
+    ]
+  },
+  {
+    question: "15 x 8 + 10 x 8 – 20 x 10?",
+    answers: [
+      {
+        type: "correct",
+        content: "0"
+      },
+      {
+        content: "1800"
+      },
+      {
+        content: "180"
+      }
+    ]
+  },
+  {
+    question:
+      "If a dozen pastries cost Rs. 24.60 then what will be the cost of 20 pastries?",
+    answers: [
+      {
+        type: "correct",
+        content: "41"
+      },
+      {
+        content: "45"
+      },
+      {
+        content: "35"
       }
     ]
   }
 ];
 
-const allotedTimeInSeconds = 90;
+const allotedTimeInSeconds = 1200;
 
 export { quizQuestions, allotedTimeInSeconds };
